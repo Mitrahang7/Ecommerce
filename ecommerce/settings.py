37412421364341868a7622df0ee4c74499df7032
Mitrahang Limbu <limbumitrahang@gmail.com>
2025-02-14 22:57:28 +0545

    changed some stuff

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -19,8 +19,8 @@ SECRET_KEY = 'django-insecure-qkmi(&mnke4kz791p=galtcwtd(#-1smst#*2lwh4b)jdgpkcy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-CSRF_TRUSTED_ORIGINS=[]
+ALLOWED_HOSTS = ['web-production-4c9f.up.railway.app', 'https://web-production-4c9f.up.railway.app']
+CSRF_TRUSTED_ORIGINS=['web-production-4c9f.up.railway.app', 'https://web-production-4c9f.up.railway.app']
 
 
 
